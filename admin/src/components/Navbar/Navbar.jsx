57065d800f3522aa5923c903468814f791341313
Manoj.A <manojamir2911@gmail.com>
2024-04-24 08:48:34 +0530

    fist commit

---
+++ admin/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+import {assets} from '../../assets/assets'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img className="logo" src={assets.logo} alt="" /><div>
+      <img className="profile" src={assets.profile_image}  alt="" />
+      <p className="admin"> Admin</p></div>
+    </div>
+  );
+};
+
+export default Navbar;
